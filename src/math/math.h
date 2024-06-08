86a53d447eeff8e7d58c493145fad3f6ef58fe17
Zbindev <zbindev@gmail.com>
2024-06-08 02:03:00 -0500

    Added Math :)

--- src/math/math.h
+++ src/math/math.h
@@ -19,6 +19,8 @@ namespace Engine {
 
         static Vector2 Add(Vector2 _a, Vector2 _b);
         static Vector2 Sub(Vector2 _a, Vector2 _b);
+        static Vector2 Mul(Vector2 _a, Vector2 _b);
+        static Vector2 Div(Vector2 _a, Vector2 _b);
         static float Distance(Vector2 _a, Vector2 _b);
         // static float Dot(Vector2 _a, Vector2 _b);
 
@@ -39,6 +41,8 @@ namespace Engine {
 
         static Vector2Int Add(Vector2Int _a, Vector2Int _b);
         static Vector2Int Sub(Vector2Int _a, Vector2Int _b);
+        static Vector2Int Mul(Vector2Int _a, Vector2Int _b);
+        static Vector2Int Div(Vector2Int _a, Vector2Int _b);
         static float Distance(Vector2Int _a, Vector2Int _b);
         // static float Dot(Vector2Int _a, Vector2Int _b);
 
